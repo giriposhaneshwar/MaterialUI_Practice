f2c367e65e8f6af598c5e3dfd23424429d0a2604
giriposhaneshwar <egp.designs@gmail.com>
2022-02-16 10:35:31 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Button } from './components/Button';
+import React, { useState } from 'react';
+import Btn from './components/Button';
 import { Routes, Route, Link } from 'react-router-dom';
 
 interface AppProps {}
@@ -7,22 +7,18 @@ interface AppState {
   name: string;
 }
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React',
-    };
-  }
+const App = (props: any) => {
+  const [name, setName] = useState('React');
 
-  render() {
-    return (
-      <div>
-        <p>Start editing to see some magic happen :)</p>
-        <Button text={this.state.name} />
-      </div>
-    );
-  }
-}
+  const handleBtnClick = () => {
+    alert(1);
+  };
+  return (
+    <div>
+      <p>Start editing to see some magic happen :)</p>
+      <Btn text={name} handleClick={handleBtnClick} />
+    </div>
+  );
+};
 
 export default App;
--- components/Button.tsx
+++ components/Button.tsx
@@ -3,6 +3,8 @@ import { ButtonGroup } from '@material-ui/core';
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-export const Button = (props: any) => {
-  return <Button>{props.text}</Button>;
+const Btn = (props: any) => {
+  return <Button onClick={props.handleClick}>{props.text}</Button>;
 };
+
+export default Btn;
