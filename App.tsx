3b57f06682445d132d5cd4385a65ef746ac3d1de
giriposhaneshwar <egp.designs@gmail.com>
2022-02-16 10:41:44 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import Btn from './components/Button';
 import { Routes, Route, Link } from 'react-router-dom';
 
@@ -7,15 +7,19 @@ interface AppState {
   name: string;
 }
 
+let c = 0;
+
 const App = (props: any) => {
+  const container = createRef();
   const [name, setName] = useState('React');
+  const [count, setCount] = useState(0);
 
   const handleBtnClick = () => {
-    alert(1);
+    container.current.innerHTML = c++;
   };
   return (
     <div>
-      <p>Start editing to see some magic happen :)</p>
+      <p ref={container}>Start editing to see some magic happen :)</p>
       <Btn text={name} handleClick={handleBtnClick} />
     </div>
   );
--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,11 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 
 const Btn = (props: any) => {
-  return <Button onClick={props.handleClick}>{props.text}</Button>;
+  return (
+    <Button variant="contained" color="primary" onClick={props.handleClick}>
+      {props.text}
+    </Button>
+  );
 };
 
 export default Btn;
